ae676ffe8ba80a2ac512d684691551598d1334e0
Nice <naserdin@protonmail.com>
2022-11-05 19:25:06 +0800

    feat: pull peer

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,15 +4,17 @@
 
 	const API_PATH = 'https://niceshit.up.railway.app';
 
-	$: streams = {};
+	let streams: any = {};
 	let myVideo: HTMLVideoElement;
 	let myStream: any;
 	let myConnId: string;
 	let connId: string;
+	let myCode: string;
 	let buttomVideo: HTMLVideoElement;
+	let peers: string[] = [];
 
 	const myPeer = new Peer({
-		secure: true,
+		secure: true
 	});
 
 	myPeer.on('open', (id) => {
@@ -26,8 +28,6 @@
 		call.on('stream', (userVideoStream) => {
 			console.log('get stream');
 			buttomVideo.srcObject = userVideoStream;
-			buttomVideo.playsInline = true;
-			buttomVideo.muted = true;
 			buttomVideo.addEventListener('loadedmetadata', () => {
 				buttomVideo.play();
 			});
@@ -35,30 +35,31 @@
 	});
 
 	onMount(() => {
-		getAllMember();
 		console.log('the component has mounted');
-		myVideo.muted = true;
-		navigator.mediaDevices
-			.getUserMedia({
-				video: true,
-				audio: true
-			})
-			.then((stream) => {
-				myStream = stream;
-				addVideoStream(myVideo, stream);
-			});
+		// navigator.mediaDevices
+		// 	.getUserMedia({
+		// 		video: true,
+		// 		audio: true
+		// 	})
+		// 	.then((stream) => {
+		// 		myStream = stream;
+		// 		addVideoStream(myVideo, stream);
+		// 	});
 	});
 
-	function RTCVideoCall() {
+	function RTCVideoCall(connId: string) {
 		const call = myPeer.call(connId, myStream);
 		call.on('stream', (userVideoStream: MediaStream) => {
 			console.log('get stream back');
 			buttomVideo.srcObject = userVideoStream;
-			buttomVideo.playsInline = true;
-			buttomVideo.muted = true;
 			buttomVideo.addEventListener('loadedmetadata', () => {
 				buttomVideo.play();
 			});
+		});
+
+		call.on('error', (error) => {
+			console.error('RTCVideoCall', error);
+			peers = peers.filter((p) => p !== connId);
 		});
 	}
 
@@ -89,23 +90,88 @@
 		navigator.clipboard.writeText(myConnId);
 	}
 
-	async function getAllMember() {
-		const res = await fetch(`${API_PATH}/member/`, {mode: 'cors'});
+	async function checkAllMember(members?: any) {
+		let membersList = members;
+		let hasEmptyPeerId = false;
+		if (!membersList) {
+			const res = await fetch(`${API_PATH}/member/`);
+			membersList = await res.json();
+		}
+		membersList.forEach((member: any) => {
+			const { peerId } = member;
+
+			if (!peerId) {
+				hasEmptyPeerId = true;
+			}
+
+			if (peerId && !peers.includes(peerId) && peerId !== myConnId) {
+				RTCVideoCall(peerId);
+				peers.push(peerId);
+			}
+		});
+
+		if (hasEmptyPeerId) {
+			setTimeout(checkAllMember, 5000);
+		}
+	}
+
+	async function checkCode() {
+		const res = await fetch(`${API_PATH}/member/${myCode}`);
+		const { error, code } = await res.json();
+		if (error) {
+			console.error('checkCode error', error);
+			return false;
+		}
+
+		if (code === myCode) {
+			console.log('checkCode pass');
+			return true;
+		}
+
+		return false;
+	}
+
+	async function joinRoom() {
+		const res = await fetch(`${API_PATH}/member/${myCode}/join/${myConnId}`, { method: 'PATCH' });
 		const data = await res.json();
-		console.log({ data });
+		console.log('join room, res', data);
+		checkAllMember(data);
+	}
+
+	async function handleJoin() {
+		if (myConnId && (await checkCode())) {
+			console.log('joined');
+			joinRoom();
+			navigator.mediaDevices
+				.getUserMedia({
+					video: true,
+					audio: true
+				})
+				.then((stream) => {
+					myStream = stream;
+					addVideoStream(myVideo, stream);
+				});
+		}
 	}
 </script>
 
 <svelte:head>
 	<title>nice shit</title>
 </svelte:head>
-<video class="video top" bind:this={myVideo} src="" on:click={switchVideoSrcObject} autoplay playsinline/>
-<video class="video" bind:this={buttomVideo} src="" autoplay playsinline />
+<video
+	class="video top"
+	bind:this={myVideo}
+	src=""
+	on:click={switchVideoSrcObject}
+	autoplay
+	playsinline
+	muted
+/>
+<video class="video" bind:this={buttomVideo} src="" autoplay playsinline muted />
 
 <div class="call">
-	<h4 on:click={copyConnId}>connection id: {myConnId}</h4>
-	<input placeholder="connection to" type="text" bind:value={connId} />
-	<button on:click={RTCVideoCall}> call </button>
+	<input placeholder="code" type="text" bind:value={myCode} />
+	<button on:click={handleJoin}> call </button>
 </div>
 
 <style>
