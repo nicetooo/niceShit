66d55531738f4ab889df9b1386f60373e56a8bbc
Nice <naserdin@protonmail.com>
2022-11-07 15:57:26 +0800

    feat: back btn added

--- src/components/Call.svelte
+++ src/components/Call.svelte
@@ -128,6 +128,25 @@
 				bind:value={$myCode}
 				autocomplete="off"
 			/>
+			<button id="back-btn" on:click={() => (location.href = '/')}>
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					xmlns:xlink="http://www.w3.org/1999/xlink"
+					version="1.1"
+					id="Capa_1"
+					viewBox="0 0 512.021 512.021"
+					style="enable-background:new 0 0 512.021 512.021;"
+					xml:space="preserve"
+					height="80%"
+					width="80%"
+				>
+					<g>
+						<path
+							d="M301.258,256.01L502.645,54.645c12.501-12.501,12.501-32.769,0-45.269c-12.501-12.501-32.769-12.501-45.269,0l0,0   L256.01,210.762L54.645,9.376c-12.501-12.501-32.769-12.501-45.269,0s-12.501,32.769,0,45.269L210.762,256.01L9.376,457.376   c-12.501,12.501-12.501,32.769,0,45.269s32.769,12.501,45.269,0L256.01,301.258l201.365,201.387   c12.501,12.501,32.769,12.501,45.269,0c12.501-12.501,12.501-32.769,0-45.269L301.258,256.01z"
+						/>
+					</g>
+				</svg>
+			</button>
 		{/if}
 		<button
 			id="call-btn"
@@ -243,6 +262,18 @@
 		border-radius: 6px;
 		width: 100px;
 		margin-right: 12px;
+		padding: 10px;
+	}
+
+	#back-btn {
+		position: fixed;
+		bottom: 20%;
+		border: none;
+		color: var(--bg-color2);
+		background-color: var(--bg-decoration);
+		border-radius: 50%;
+		width: 60px;
+		height: 60px;
 		padding: 10px;
 	}
 
