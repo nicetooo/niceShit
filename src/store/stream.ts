e62d0661dfe6341404f9d6dde89a92d6d04e3944
Nice <naserdin@protonmail.com>
2022-11-10 00:18:25 +0800

    chore: up

--- src/store/stream.ts
+++ src/store/stream.ts
@@ -2,6 +2,8 @@ import type Peer from 'peerjs';
 import type { Socket } from 'socket.io-client';
 import { readable, writable } from 'svelte/store';
 
+//http://localhost:5173/index-153baff3
+
 export const next = readable('index-153baff3');
 export const myCode = writable('');
 export const myConnId = writable('');
